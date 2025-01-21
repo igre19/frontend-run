eebde43dca98a55796040e516f35a7759be4d6a1
Marin <budicmarin@gmail.com>
2025-01-21 12:09:51 +0100

    nOVO

--- src/store.js
+++ src/store.js
@@ -1,13 +1,39 @@
-export default {
-    searchTerm: "",
-    currentUser: null,
-    isAdmin: false,
+import { reactive } from "vue";
 
-    checkAdmin() {
-        if (this.currentUser && this.currentUser.email === "plese@gmail.com") {
-            this.isAdmin = true;
-        } else {
-            this.isAdmin = false;
-        }
-    },
+// Inicijalizacija state objekta kao reaktivnog
+const state = reactive({
+  token: localStorage.getItem("token") || null,
+});
+
+// Funkcija za pohranu tokena
+
+export default {
+  searchTerm: "",
+  isAdmin: false,
+  currentUser: {
+    id: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  },
+  state, // Izvozimo state koji je sada reaktivan
+  saveToken(token) {
+    localStorage.setItem("token", token);
+    state.token = token; // Ovdje se koristi reaktivni state
+  },
+
+  // Funkcija za brisanje tokena
+  clearToken() {
+    localStorage.removeItem("token");
+    state.token = null; // Ovdje se također koristi reaktivni state
+  },
+
+  checkAdmin() {
+    if (this.currentUser && this.currentUser.email === "plese@gmail.com") {
+      this.isAdmin = true;
+    } else {
+      this.isAdmin = false;
+    }
+  },
 };
