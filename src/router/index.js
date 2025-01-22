197bfb35061682d09cc37a66a37dad80f0bcdb60
Marin <budicmarin@gmail.com>
2025-01-22 11:51:44 +0100

    dodavanje utrka bez slike

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import LoginView from "../views/LoginView.vue";
@@ -10,81 +10,79 @@ import UrediUtrku from "@/views/UrediUtrkuView.vue";
 import FavoritesView from "../views/FavoritesView.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        name: "home",
-        component: HomeView,
-        meta: { needsUser: false },
-    },
-    {
-        path: "/about",
-        name: "about",
-        component: AboutView,
-        meta: { needsUser: false },
-    },
-    {
-        path: "/favorites",
-        name: "favorites",
-        component: FavoritesView,
-        meta: { needsUser: true },
-    },
-    {
-        path: '/login',
-        name: "login",
-        component: LoginView,
-        meta: { guestOnly: true },
-    },
-    {
-        path: '/signup',
-        name: "signup",
-        component: SignupView,
-        meta: { guestOnly: true },
-    },
-    {
-        path: "/profile",
-        name: "user-profile",
-        component: UserProfile,
-        meta: { needsUser: true },
-    },
-    {
-        path: "/newraces",
-        name: "new-races",
-        component: NewRace,
-        meta: { needsUser: true, adminOnly: true }, // Samo admini mogu pristupiti
-    },
-    {
-        path: "/uredi-utrku/:id",
-        name: "uredi-utrku",
-        component: UrediUtrku,
-        meta: { needsUser: true, adminOnly: true },
-    },
-    {
-        path: "/:catchAll(.*)",
-        redirect: "/",
-    }
+  {
+    path: "/",
+    name: "Home",
+    name: "home",
+    component: HomeView,
+    meta: { needsUser: false },
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: AboutView,
+    meta: { needsUser: false },
+  },
+  {
+    path: "/favorites",
+    name: "favorites",
+    component: FavoritesView,
+    meta: { needsUser: true },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+    meta: { guestOnly: true },
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignupView,
+    meta: { guestOnly: true },
+  },
+  {
+    path: "/profile",
+    name: "user-profile",
+    component: UserProfile,
+    meta: { needsUser: true },
+  },
+  {
+    path: "/newraces",
+    name: "new-races",
+    component: NewRace,
+    meta: { needsUser: true }, // Samo admini mogu pristupiti
+  },
+  {
+    path: "/uredi-utrku/:id",
+    name: "uredi-utrku",
+    component: UrediUtrku,
+    meta: { needsUser: true, adminOnly: true },
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/",
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
+  const isLoggedIn = store.currentUser !== null;
+  const isAdmin = store.isAdmin;
 
-    const isLoggedIn = store.currentUser !== null;
-    const isAdmin = store.isAdmin;
-
-    if (isLoggedIn && to.meta.guestOnly) {
-        next({ name: "home" });
-    } else if (!isLoggedIn && to.meta.needsUser) {
-        next({ name: "login" });
-    } else if (to.meta.adminOnly && !isAdmin) {
-        next({ name: "home" }); // Preusmjeri usere koji nisu admini
-    } else {
-        next();
-    }
+  if (isLoggedIn && to.meta.guestOnly) {
+    next({ name: "home" });
+  } else if (!isLoggedIn && to.meta.needsUser) {
+    next({ name: "login" });
+  } else if (to.meta.adminOnly && !isAdmin) {
+    next({ name: "home" }); // Preusmjeri usere koji nisu admini
+  } else {
+    next();
+  }
 });
-
 
 export default router;
